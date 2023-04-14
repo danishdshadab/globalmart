1d5b6ee6da6354c5472c3e745c59527498762926
danishdshadab@gmail.com <danishdshadab@gmail.com>
2023-04-14 06:46:55 +0000

    develop stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+Select 
+--from raw order
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+ordersellingprice - ordercostprice as orderprofit,
+o.ordersellingprice,
+ordercostprice,
+-- from raw customer
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+    on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+    on o.productid = p.productid
+
